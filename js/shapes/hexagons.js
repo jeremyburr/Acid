011e2da4df18d0a348aa00901679067bb633e74e
jerburr <jeremy.burr@weareheartbeat.com>
2022-04-08 01:32:26 -0400

    Refactor - prepare to convert hexagon-from-triangles example to representation in hexagons JS by way of buffer geometry

--- js/shapes/hexagons.js
+++ js/shapes/hexagons.js
@@ -6,7 +6,7 @@ import {
 	DoubleSide,
 } from '../three.module.js';
 
-    // Inner
+    // Top
 
     const shapeHexTop = new Shape();
     shapeHexTop.moveTo(0,0);
@@ -17,6 +17,8 @@ import {
     shapeHexTop.lineTo(Math.sqrt(3),1)
     const geometryHexTop = new ShapeGeometry( shapeHexTop );
 
+    // Bottom
+
     const shapeHexBottom = new Shape();
     shapeHexBottom.moveTo(0,0);
     shapeHexBottom.lineTo(Math.sqrt(3),-1)
@@ -26,6 +28,17 @@ import {
     shapeHexBottom.lineTo(-Math.sqrt(3),-1)
     const geometryHexBottom = new ShapeGeometry( shapeHexBottom );
 
+    // Back
+    
+    const shapeHexBack = new Shape();
+    shapeHexBack.moveTo(0,0);
+    shapeHexBack.lineTo(Math.sqrt(3),-1)
+    shapeHexBack.lineTo(Math.sqrt(3),-3)
+    shapeHexBack.lineTo(0,-4)
+    shapeHexBack.lineTo(-Math.sqrt(3),-3)
+    shapeHexBack.lineTo(-Math.sqrt(3),-1)
+    const geometryHexBack = new ShapeGeometry( shapeHexBack );
+
     const greenMaterial = new MeshBasicMaterial({ 
         color: 0x00ff00,
         /*transparent: true, opacity: .1*/
@@ -34,26 +47,6 @@ import {
 
     const Top = new Mesh( geometryHexTop, greenMaterial ) ;
     const Bottom = new Mesh( geometryHexBottom, greenMaterial ) ;
-
-    // Outer
-
-    const shapeHexBack = new Shape();
-    /*shapeHexBack.moveTo(0,0);
-    shapeHexBack.lineTo(-Math.sqrt(3),1)
-    shapeHexBack.lineTo(-Math.sqrt(3),3)
-    shapeHexBack.lineTo(0,4)
-    shapeHexBack.lineTo(Math.sqrt(3),3)
-    shapeHexBack.lineTo(Math.sqrt(3),1)*/
-    const geometryHexBack = new ShapeGeometry( shapeHexBack );
-
-    const blueMaterial = new MeshBasicMaterial({ 
-        color: 0x0000FF,
-        /*transparent: true, opacity: .1*/
-    });
-
-    blueMaterial.side = DoubleSide;
-
-    const Back = new Mesh( geometryHexBack, blueMaterial ) ;
-
+    const Back = new Mesh( geometryHexBack, greenMaterial ) ;
 
     export {Top, Bottom, Back}
