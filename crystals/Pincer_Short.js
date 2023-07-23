5920abf096054e5928d83db6b643afc83436a80e
jerburr <jeremyburr@users.noreply.github.com>
2023-07-23 11:38:16 -0400

    Pincer short top base

--- crystals/Pincer_Short.js
+++ crystals/Pincer_Short.js
@@ -339,6 +339,67 @@ const endPoint1XNueYNegZNeg = returnEndPosition(
     "thin"
   );
 
+  // Extend Y+X+Z+
+
+  // Top
+  addLine(
+    [endPoint1XPosYNeg[0] + 2 * unitLength, orgY + 2 * unitLength, orgZ],
+    [orgX, orgY + 2 * unitLength, endPoint1XNueYNegZPos[2] + 2 * unitLength],
+    "thin"
+  );
+  // Bottom
+  addLine(
+    [endPoint1XPosYNeg[0] + unitLength * 4, orgY + unitLength * 4.25, orgZ],
+    [orgX, orgY + unitLength * 4.25, endPoint1XNueYNegZPos[2] + unitLength * 4],
+    "thin"
+  );
+
+  // Extend Y-Z+X-
+
+  //Top
+  addLine(
+    [orgX, orgY + 2 * unitLength, endPoint1XNueYNegZPos[2] + 2 * unitLength],
+    [endPoint1XNegYNeg[0] - 2 * unitLength, orgY + 2 * unitLength, orgZ],
+    "thin"
+  );
+  //Bottom
+  addLine(
+    [orgX, orgY + unitLength * 4.25, endPoint1XNueYNegZPos[2] + unitLength * 4],
+    [endPoint1XNegYNeg[0] - unitLength * 4, orgY + unitLength * 4.25, orgZ],
+    "thin"
+  );
+
+  // Extend Y-X-Z- 
+
+  //Top
+  addLine(
+    [endPoint1XNegYNeg[0] - 2 * unitLength, orgY + 2 * unitLength, orgZ],
+    [orgX, orgY + 2 * unitLength, endPoint1XNueYNegZNeg[2] - 2 * unitLength],
+    "thin"
+  );
+  //Bottom
+  addLine(
+    [endPoint1XNegYNeg[0] - unitLength * 4, orgY + unitLength * 4.25, orgZ],
+    [orgX, orgY + unitLength * 4.25, endPoint1XNueYNegZNeg[2] - unitLength * 4],
+    "thin"
+  );
+
+  // Extend Y-Z- 
+
+  //Top
+  addLine(
+    [orgX, orgY + 2 * unitLength, endPoint1XNueYNegZNeg[2] - 2 * unitLength],
+    [endPoint1XPosYNeg[0] + 2 * unitLength, orgY + 2 * unitLength, orgZ],
+    "thin"
+  );
+  //Bottom
+  addLine(
+    [orgX, orgY + unitLength * 4.25, endPoint1XNueYNegZNeg[2] - unitLength * 4],
+    [endPoint1XPosYNeg[0] + unitLength * 4, orgY + unitLength * 4.25, orgZ],
+    "thin"
+  );
+
+
 
   //  X Nue Y Neg Z Pos
 
