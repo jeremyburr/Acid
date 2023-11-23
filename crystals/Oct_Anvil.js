8f32f1dc5b969e90798929848694d31d1d682526
jerburr <jeremyburr@users.noreply.github.com>
2023-11-22 21:37:16 -0500

    Oct Anvil X+Z-

--- crystals/Oct_Anvil.js
+++ crystals/Oct_Anvil.js
@@ -123,7 +123,7 @@ function Oct_Anvil(origin, unitLength, position) {
   addVectorLine(
     'L1_X+Y+Z+',
     1,
-    [1,1,0],
+    [1,1,1],
     [
       orgX + Math.cos(degreesToRadians(45)) * unitLength,
       orgY + unitLength,
@@ -177,7 +177,7 @@ function Oct_Anvil(origin, unitLength, position) {
   addVectorLine(
     'L1_X+Y-Z+',
     1,
-    [1,-1,0],
+    [1,-1,1],
     [
       orgX + Math.cos(degreesToRadians(45)) * unitLength,
       orgY - unitLength,
@@ -427,6 +427,114 @@ function Oct_Anvil(origin, unitLength, position) {
       orgZ - 2 * unitLength
     ],
     [90,135,135],
+    unitLength,
+    unitLength,
+    "curved"
+  )
+
+  // Anvil X+Z-
+
+  // Y+
+
+  // L1 
+
+  addVectorLine(
+    'L1_X+Y+Z-',
+    1,
+    [1,1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * unitLength,
+      orgY + unitLength,
+      orgZ - Math.cos(degreesToRadians(45))* unitLength
+    ],
+    [45,90,135],
+    unitLength,
+    0
+  )
+
+
+  // L2 
+
+  addVectorLine(
+    'L2_X+Y+Z-',
+    2,
+    [1,1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * 2 * unitLength,
+      orgY + unitLength,
+      orgZ - Math.cos(degreesToRadians(45)) * 2 * unitLength
+    ],
+    [90,0,90],
+    unitLength,
+    0
+  )
+
+
+  // L3 
+
+  generateCurveFromVector(
+    'L3_X+Y+Z-',
+    3,
+    [1,1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * 2 * unitLength,
+      orgY + 2 * unitLength,
+      orgZ - Math.cos(degreesToRadians(45)) * 2 * unitLength
+    ],
+    [45,45,135],
+    unitLength,
+    unitLength,
+    "curved"
+  )
+
+
+  // Y-
+
+  // L1 
+
+  addVectorLine(
+    'L1_X+Y-Z-',
+    1,
+    [1,-1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * unitLength,
+      orgY - unitLength,
+      orgZ - Math.cos(degreesToRadians(45))* unitLength
+    ],
+    [45,90,135],
+    unitLength,
+    0
+  )
+
+
+  // L2 
+
+  addVectorLine(
+    'L2_X+Y-Z-',
+    2,
+    [1,-1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * 2 * unitLength,
+      orgY - unitLength,
+      orgZ - Math.cos(degreesToRadians(45)) * 2 * unitLength
+    ],
+    [90,180,90],
+    unitLength,
+    0
+  )
+
+  // L3 
+
+  generateCurveFromVector(
+    'L3_X+Y-Z-',
+    3,
+    [1,-1,-1],
+    [
+      orgX + Math.cos(degreesToRadians(45)) * 2 * unitLength,
+      orgY - 2 * unitLength,
+      orgZ - Math.cos(degreesToRadians(45)) * 2 * unitLength
+    ],
+    [45,135,135],
     unitLength,
     unitLength,
     "curved"
