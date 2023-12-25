2293b23d250a10e739344b65fdd547f807eae42c
jerburr <jeremyburr@users.noreply.github.com>
2023-12-25 01:33:30 -0500

    Initial 16N mappings

--- js/ReflectionMappings.js
+++ js/ReflectionMappings.js
@@ -70,6 +70,116 @@ function Compute_Reflection_Mappings(level, unitLength, magnitude, origin) {
       magnitude: unitLength,
       unitPosition: 0,
     },
+    'X_Y+Z-': {
+      notation: [0, 1, -1],
+      direction: [90, 45, 180],
+      startPosition: [
+        origin[0],
+        origin[1] + unitLength,
+        origin[2] - unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X_Y-Z-': {
+      notation: [0, -1, -1],
+      direction: [90, 135, 180],
+      startPosition: [
+        origin[0],
+        origin[1] - unitLength,
+        origin[2] - unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y+Z-': {
+      notation: [-1, 1, -1],
+      direction: [135, 45, 135],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] + unitLength,
+        origin[2] - unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y-Z-': {
+      notation: [-1, -1, -1],
+      direction: [135, 135, 135],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] - unitLength,
+        origin[2] - unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y+Z_': {
+      notation: [-1, 1, 0],
+      direction: [180, 45, 90],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] + unitLength,
+        origin[2] 
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y-Z_': {
+      notation: [-1, -1, 0],
+      direction: [180, 135, 90],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] - unitLength,
+        origin[2] 
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y+Z+': {
+      notation: [-1, 1, +1],
+      direction: [135, 45, 45],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] + unitLength,
+        origin[2] + unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X-Y-Z+': {
+      notation: [-1, -1, +1],
+      direction: [135, 135, 45],
+      startPosition: [
+        origin[0] - unitLength,
+        origin[1] - unitLength,
+        origin[2] + unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X_Y+Z+': {
+      notation: [0, 1, +1],
+      direction: [90, 45, 0],
+      startPosition: [
+        origin[0],
+        origin[1] + unitLength,
+        origin[2] + unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
+    'X_Y-Z+': {
+      notation: [0, -1, +1],
+      direction: [90, 135, 0],
+      startPosition: [
+        origin[0],
+        origin[1] - unitLength,
+        origin[2] + unitLength
+      ],
+      magnitude: unitLength,
+      unitPosition: 0,
+    },
   }
 }
 
