733f0d530f7af8eabef93e3047926253c4272f2d
jerburr <jeremyburr@users.noreply.github.com>
2024-01-01 01:31:04 -0500

    Slight Refactor Reflection mappings

--- js/ReflectionMappings.js
+++ js/ReflectionMappings.js
@@ -2,61 +2,27 @@ const unitLength = 20;
 
 function Compute_Reflection_Mappings(unitLength, origin, offset) {
 
-  const [offsetX, offsetY, offsetZ] = offset;
-
   const coef2D = 1/Math.sqrt(2);
   const coef3D = 1/Math.sqrt(3);
 
   const mappings = {
 
-    'X+Y+Z+': {
-      orientation: [1,1,1],
-    },
-    'X+Y-Z+': {
-      orientation: [1,-1,1],
-    },
-    'X+Y+Z_': {
-      orientation: [1, 1, 0],
-    },
-    'X+Y-Z_': {
-      orientation: [1, -1, 0],
-    },
-    'X+Y+Z-': {
-      orientation: [1, 1, -1],
-    },
-    'X+Y-Z-': {
-      orientation: [1, -1, -1],
-    },
-    'X_Y+Z-': {
-      orientation: [0, 1, -1],
-    },
-    'X_Y-Z-': {
-      orientation: [0, -1, -1],
-    },
-    'X-Y+Z-': {
-      orientation: [-1, 1, -1],
-    },
-    'X-Y-Z-': {
-      orientation: [-1, -1, -1],
-    },
-    'X-Y+Z_': {
-      orientation: [-1, 1, 0],
-    },
-    'X-Y-Z_': {
-      orientation: [-1, -1, 0],
-    },
-    'X-Y+Z+': {
-      orientation: [-1, 1, 1],
-    },
-    'X-Y-Z+': {
-      orientation: [-1, -1, 1],
-    },
-    'X_Y+Z+': {
-      orientation: [0, 1, 1],
-    },
-    'X_Y-Z+': {
-      orientation: [0, -1, 1],
-    },
+    'X+Y+Z+': { orientation: [1,1,1]},
+    'X+Y-Z+': { orientation: [1,-1,1]},
+    'X+Y+Z_': { orientation: [1,1,0]},
+    'X+Y-Z_': { orientation: [1,-1,0]},
+    'X+Y+Z-': { orientation: [1,1,-1]},
+    'X+Y-Z-': { orientation: [1,-1,-1]},
+    'X_Y+Z-': { orientation: [0,1,-1]},
+    'X_Y-Z-': { orientation: [0,-1,-1]},
+    'X-Y+Z-': { orientation: [-1,1,-1]},
+    'X-Y-Z-': { orientation: [-1,-1,-1]},
+    'X-Y+Z_': { orientation: [-1,1,0]},
+    'X-Y-Z_': { orientation: [-1,-1,0]},
+    'X-Y+Z+': { orientation: [-1,1,1]},
+    'X-Y-Z+': { orientation: [-1,-1,1]},
+    'X_Y+Z+': { orientation: [0,1,1]},
+    'X_Y-Z+': { orientation: [0,-1,1]},
   }
 
   for (const mapping in mappings) {
